f5523ff198669cc1ef12170e35c4bf421af4bb9d
avikaps <avi.kaps@gmail.com>
2014-05-12 15:54:29 +0530

    Created Modules in Solutions.py

--- Solution.py
+++ Solution.py
@@ -1,26 +1,29 @@
 import csv
 from collections import OrderedDict, namedtuple
 
-# Open a file as a read_file
-with open('sample.csv') as r_file:
-    reader = csv.reader(r_file)
-    # Create an output tuple !! 
-    out_tuple = namedtuple('Output', ['Highest_Price', 'Year', 'Month'])
-    # Create an empty ordered dictionary
-    Ordr_Dict = OrderedDict()
-    # Get all the company names after index 2
-    Company_Names = next(reader)[2:]
-    # Add those names into ordered dictionary
-    for Company in Company_Names:
-        Ordr_Dict[Company] = out_tuple(0, 'Year', 'Month')
-    # Read the csv row by row
-    for row in reader:
-        year, month = row[:2]
-        for Company, price in zip(Company_Names, map(int, row[2:])):
-            #Check if the price in the ordered dictionay is higher
-            if Ordr_Dict[Company].Highest_Price < price:
-                Ordr_Dict[Company] = out_tuple(price, year, month)
+def process_csv(filename):
+    # Open a file as a read_file
+    with open(filename) as r_file:
+        reader = csv.reader(r_file)
+        # Create an output tuple !! 
+        out_tuple = namedtuple('Output', ['Highest_Price', 'Year', 'Month'])
+        # Create an empty ordered dictionary
+        Ordr_Dict = OrderedDict()
+        # Get all the company names after index 2
+        Company_Names = next(reader)[2:]
+        # Add those names into ordered dictionary
+        for Company in Company_Names:
+            Ordr_Dict[Company] = out_tuple(0, 'Year', 'Month')
+        # Read the csv row by row
+        for row in reader:
+            year, month = row[:2]
+            for Company, price in zip(Company_Names, map(int, row[2:])):
+                #Check if the price in the ordered dictionay is higher
+                if Ordr_Dict[Company].Highest_Price < price:
+                    Ordr_Dict[Company] = out_tuple(price, year, month)
+    display_output(Ordr_Dict)
 
-# Iterate over ordered dictionary to get fancier output
-for items in Ordr_Dict.iteritems():
-    print items
+def display_output(Ordr_Dict):
+    # Iterate over ordered dictionary to get fancier output
+    for items in Ordr_Dict.iteritems():
+        print items
